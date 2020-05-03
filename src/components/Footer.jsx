6c8aeff9c71bce802391bb6b962f823483c3b6c6
osaro2020 <58718596+osaro2020@users.noreply.github.com>
2020-05-02 22:24:11 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year} appByOsaro</p>
+      <p>Copyright ⓒ {year} appByOsas</p>
     </footer>
   );
 }
